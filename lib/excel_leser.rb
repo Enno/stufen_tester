5f14e05b495658205464c16b075f8c062b8a414c
Enno <too_lame_for_a_name@gmx.net>
2009-07-29 22:16:18 +0200

    noch nichts wirklich spruchreifes... morgen wirds besser (?!)

--- lib/excel_leser.rb
+++ lib/excel_leser.rb
@@ -1,7 +1,94 @@
 
+#constant definition
+FILE_PATH = File.dirname(File.dirname(__FILE__)) +  '/daten/'
 
-class ExcelLeser
-  def initialize(mappen_name, global_blatt_name, tabellen_name)
-    
+class ExcelInputBox # spaeter ersetzen durch rwd http://www.erikveen.dds.nl/rubywebdialogs/index.html#1.0.0
+  require 'win32ole'
+  def initialize
+    @user_iput = ""
+  end
+  def get_user_input(prompt='')
+    #@user_input = ""
+    until @user_input.is_a?(String) && @user_input.length > 0 do
+      excel_input_box = WIN32OLE.new('Excel.Application')
+      @user_input = excel_input_box.InputBox(prompt)
+      excel_input_box.Quit
+      excel_input_box = nil
+    end
+    return @user_input
   end
 end
+class ExcelController
+  require 'win32ole'
+  attr_accessor :excel_file_name, :excel_sheet_name
+  attr_reader :workbook, :sheet
+  def initialize()
+    @excel_file_name = ""
+    @excel_appl = ""
+    @workbook = ""
+    @sheet = Array.new
+    sheet_count = 0
+  end
+  def open_excel_file()
+    if File.exist?(@excel_file_name)
+      @excel_appl = WIN32OLE.new('Excel.Application')
+      @excel_appl['Visible'] = true
+      @excel_appl.Workbooks.Open(FILE_PATH + @excel_file_name)
+      @workbook = WIN32OLE.connect('Excel.Application').ActiveWorkbook #problem wenn alter excel task noch aktiv
+    else
+      raise "Error Message: " "Datei nicht vorhanden" # eventuell neue abfrage
+    end
+  end
+  def find_excel_sheet
+    sheet_count = @excel_appl.Worksheets.count
+    for i in (1..sheet_count) do #@excel_appl.Worksheets.each) do
+      @sheet << @excel_appl.Worksheets(i).Name
+    end
+    return @sheet
+  end
+  def select_excel_sheet
+    if @sheet.include?(@excel_sheet_name)
+    puts @excel_sheet_name
+    @excel_appl.Worksheets(@excel_sheet_name).select
+    else
+      raise "Error Message: " "Sheet nicht vorhanden" # eventuell neue abfrage
+    end
+  end
+  def close_excel_file()
+    @excel_appl.ActiveWorkbook.Close(0)
+  end
+  def quit_excel
+    @excel_appl.quit
+    @excel_appl = nil
+    GC.start #garbage control starten
+  end
+end
+class ExcelReader
+  require 'win32ole'
+  def initialize()
+
+  end
+  def get_contents
+    
+  end
+
+end
+
+
+excel = ExcelController.new
+
+excel.excel_file_name = ExcelInputBox.new.
+  get_user_input("Welche Datei soll geoeffnet werden?") + '.xls'
+
+excel.open_excel_file
+
+excel.excel_sheet_name = ExcelInputBox.new.
+  get_user_input("Welches Worksheet soll geoeffnet werden? (" +
+    excel.find_excel_sheet.collect { |names| names + ", "}.to_s.chop.chop + ")")
+
+excel.select_excel_sheet
+
+sleep(2)
+excel.close_excel_file
+sleep(2)
+excel.quit_excel
--- spec/excel_leser_spec.rb
+++ spec/excel_leser_spec.rb
@@ -1,33 +1,56 @@
 
-require 'lib/excel_leser'
+require 'win32ole'
+require File.dirname(File.dirname(__FILE__)) +  '/../lib/excel_leser.rb'
+require 'spec'
 
-describe ExcelLeser do
-  before(:each) do
-    mappen_name = "Tabelle-4sr_a.xls"
-    mappen_pfad = File.dirname(File.dirname(__FILE__)) +  "/daten/"
-    @el = ExcelLeser.new(mappen_pfad + mappen_name, "Global", "Tabelle")
+describe ExcelInputBox do
+  it "Excel-Eingabe-Box sollte nicht leer sein" do
+    @user_input.should_not be_nil
+    puts "OK"
   end
-
-  it "sollte existieren" do
-    @el.should_not be_nil
+  it "Excel-Eingabe-Box sollte ein string sein" do
+    @user_input.is_a?(String)
   end
-
-  it "sollte auf die Methode 'zeile' reagieren" do
-    @el.zeile(22).should_not be_nil
-  end
-
-  it "sollte Zeile 22 korrekt einlesen" do
-    z22 = @el.zeile(22)
-
-    z22[:name].should                  == "Hans Meier"
-    z22[:verzicht_als_netto].should    == 20.0
-  end
-
-  it "sollte Zeile 21 korrekt einlesen" do
-    z21 = @el.zeile(21)
-
-    z21[:name].should                  == "Gerda Müller"
-    z21[:berufsgruppe].should          == "Angestellte/Arbeiter"
-  end
+  #TODO
 end
+#describe ExcelController do
+#  before(:each) do
+#    excel_file_name = File.dirname(File.dirname(__FILE__)) +  '/daten/' + "test.xls"
+#    #@excel_appl = ExcelController.new + excel_file_name
+#  end
+#  it "Excel-Datei sollte geoeffnet sein" do
+#    excel_file_name.should_not be_nil
+#  end
+#
+#  #TODO
+#end
+#describe ExcelLeser do
+#  before(:each) do
+#    mappen_name = "Tabelle-4sr_a.xls"
+#    mappen_pfad = File.dirname(File.dirname(__FILE__)) +  "/daten/"
+#    @el = ExcelLeser.new(mappen_pfad + mappen_name, "Global", "Tabelle")
+#  end
+#
+#  it "sollte existieren" do
+#    @el.should_not be_nil
+#  end
+#
+#  it "sollte auf die Methode 'zeile' reagieren" do
+#    @el.zeile(22).should_not be_nil
+#  end
+#
+#  it "sollte Zeile 22 korrekt einlesen" do
+#    z22 = @el.zeile(22)
+#
+#    z22[:name].should                  == "Hans Meier"
+#    z22[:verzicht_als_netto].should    == 20.0
+#  end
+#
+#  it "sollte Zeile 21 korrekt einlesen" do
+#    z21 = @el.zeile(21)
+#
+#    z21[:name].should                  == "Gerda Müller"
+#    z21[:berufsgruppe].should          == "Angestellte/Arbeiter"
+#  end
+#end
 
