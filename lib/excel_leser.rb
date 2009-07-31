e2ba0fd3ec1452bb5c492035b4194691381694b2
Enno <too_lame_for_a_name@gmx.net>
2009-07-31 08:38:31 +0200

    3 tests laufen:
    code wurde abgespeckt

--- lib/excel_leser.rb
+++ lib/excel_leser.rb
@@ -20,158 +20,78 @@ class ExcelInputBox # spaeter ersetzen durch rwd http://www.erikveen.dds.nl/ruby
 end
 class ExcelController
   require 'win32ole'
-  attr_accessor :excel_file_name, :excel_sheet_name
+  attr_accessor :excel_datei_name, :excel_blatt_name
   attr_reader :workbook, :sheet, :current_sheet_name
   def initialize(pfad, blatt1, blatt2)
-    @excel_file_name = ""
-    @excel_sheet_name = ""
+    @excel_datei_name = ""
+    @excel_blatt_name = ""
     @excel_appl = ""
-    @workbook = ""
+    @mappe = ""
     @sheet = Array.new
     sheet_count = 0
     @@current_sheet_name = ""
-    @@current_excel_appl = ""
   end
   def open_excel_file(pfad)
-    #dateiname = FILE_PATH + @excel_file_name
+    #dateiname = FILE_PATH + @excel_datei_name
     dateiname = pfad
-    if File.exist?(dateiname) #(@excel_file_name)
+    if File.exist?(dateiname) #(@excel_datei_name)
       #      @excel_appl = WIN32OLE.GetActiveObject('Excel.Application') ||
       #        WIN32OLE.new('Excel.Application')
       @excel_appl = WIN32OLE.new('Excel.Application') 
       @excel_appl['Visible'] = true      
       @excel_appl.Workbooks.Open(dateiname)
-      @@current_excel_appl = @excel_appl
-      @workbook = WIN32OLE.connect('Excel.Application').ActiveWorkbook #problem wenn alter excel task noch aktiv
+      @mappe = WIN32OLE.connect('Excel.Application').ActiveWorkbook #problem wenn alter excel task noch aktiv
     else
       raise "Error Message: " "Datei '#{dateiname}' nicht vorhanden " # eventuell neue abfrage
     end
   end
-  def find_excel_sheet
-    sheet_count = @excel_appl.Worksheets.count
-    for i in (1..sheet_count) do #@excel_appl.Worksheets.each) do
-      @sheet << @excel_appl.Worksheets(i).Name
-    end
-    return @sheet
-  end
-  def select_excel_sheet
-    if @sheet.include?(@excel_sheet_name)
-      @excel_appl.Worksheets(@excel_sheet_name).select
-      @@current_sheet_name = @excel_sheet_name
-      return @@current_sheet_name
-    else
-      raise "Error Message: " "Sheet nicht vorhanden" # eventuell neue abfrage
-    end
-  end
-  def close_excel_file()
-    @excel_appl.ActiveWorkbook.Close(0)
-  end
+#  def find_excel_sheet
+#    sheet_count = @excel_appl.Worksheets.count
+#    for i in (1..sheet_count) do #@excel_appl.Worksheets.each) do
+#      @sheet << @excel_appl.Worksheets(i).Name
+#    end
+#    return @sheet
+#  end
+#  def select_excel_sheet
+#    if @sheet.include?(@excel_blatt_name)
+#      @excel_appl.Worksheets(@excel_blatt_name).select
+#      @@current_sheet_name = @excel_blatt_name
+#      return @@current_sheet_name
+#    else
+#      raise "Error Message: " "Sheet nicht vorhanden" # eventuell neue abfrage
+#    end
+#  end
+#  def close_excel_file()
+#    @excel_appl.ActiveWorkbook.Close(0)
+#  end
   def quit_excel
     @excel_appl.quit
     @excel_appl = nil
     GC.start #garbage control starten
   end
 end
-# person + personalnr zur identifikation, falls doppelte namen
-# spaeter abfrage welchen datensatz auslesen
+
 class ExcelLeser < ExcelController
   require 'win32ole'
   require 'stufen_tester'
   attr_accessor :column_name, :first_std_column
   def initialize(pfad, global_name, tabelle_name)
-    @start_row = 1
-    @start_colum = 1
     @start_addresse = nil
-    @row_offset = 2
-    @column_offset = 0
-    @column_name = ""
-    @one_dataset = []
-    @current_value = []
+    @zeilen_offset = 2
     i = 0
-    @first_std_column = ""
     @tabelle_name = tabelle_name
     super
     open_excel_file(pfad)
    end
-  def get_contents
-    WIN32OLE.connect('Excel.Application').ActiveWorkbook
-    if @@current_excel_appl.
-        WorkSheets(@@current_sheet_name).Cells(@start_row, @start_colum).
-        find(@column_name[0..6])
-      @start_address = @@current_excel_appl.
-        WorkSheets(@@current_sheet_name).Cells(@start_row, @start_colum).
-        find(@column_name[0..6]).address
-      @current_value = @@current_excel_appl.WorkSheets(@@current_sheet_name).
-        range(@start_address).offset(@row_offset,@column_offset).value
-      if @current_value == nil
-        raise "Error Message: " "Kein Datensatz vorhanden."
-      else
-        i=0
-        loop do #bessere schleife einbauen, sonst fehler beim letzten durchgang (all)
-          @current_value = @@current_excel_appl.WorkSheets(@@current_sheet_name).
-            range(@start_address).offset(@row_offset+i,@column_offset).value
-          i+=1
-          @one_dataset << @current_value
-          break if @current_value == nil
-        end
-      end
-      return @one_dataset
-    else
-      raise "Error Message :" "Spaltenname nicht gefunden"
-    end
-  end
   def zeile(zeilen_nummer)
     i=0
-    loop do #bessere schleife einbauen, sonst fehler beim letzten durchgang (all)
+    loop do 
       @aktuelle_zeile = @excel_appl.WorkSheets(@tabelle_name).
-        range("A1").offset(zeilen_nummer,@column_offset+i).value
+        range("A1").offset(zeilen_nummer,i).value
       i+=1
-      return @aktuelle_zeile
+      
       break if @aktuelle_zeile == nil
     end
   end
 end
 
-#excel = ExcelController.new
-#
-#excel.excel_file_name = ExcelInputBox.new.
-#  get_user_input("Welche Datei soll geoeffnet werden?") + '.xls'
-
-#excel.open_excel_file
-#
-#excel.excel_sheet_name = ExcelInputBox.new.
-#  get_user_input("Welches Worksheet soll geoeffnet werden? (" +
-#    excel.find_excel_sheet.collect { |names| names + ", "}.to_s.chop.chop + ")")
-#
-#excel.select_excel_sheet
-#
-#datasets = ExcelLeser.new
-#
-#column_name = ExcelInputBox.new.
-#  get_user_input("Welche Spalte soll ausgelesen werden?") # pulldown menue benoetigt
-#if SPALTEN_UEBERSCHRIFTEN.include?(column_name.to_sym)
-#  datasets.column_name = SPALTEN_UEBERSCHRIFTEN["#{column_name}".to_sym]
-#  datasets = datasets.get_contents
-#  # puts datasets
-#elsif column_name == "all"
-#  i = 0
-#  data_hash = {}
-#  SPALTEN_UEBERSCHRIFTEN.each do |key, value|
-#    datasets.column_name = value
-#    data_hash[key] = datasets.get_contents
-#    i += 1
-#    puts value
-#    puts i
-#  end
-#else
-#  raise "Error Message :" "Spaltenname existiert nicht"
-#end
-#
-#excel = ExcelController.new
-#mappen_pfad = File.dirname(File.dirname(__FILE__)).gsub('\\', '/') +  '/daten/'
-#excel.open_excel_file(mappen_pfad + mappen_name, "Global", "Tabelle")
-#
-#sleep(2)
-#excel.close_excel_file
-#sleep(2)
-#excel.quit_excel
--- spec/excel_leser_spec.rb
+++ spec/excel_leser_spec.rb
@@ -2,28 +2,7 @@
 require 'win32ole'
 require File.dirname(File.dirname(__FILE__)) +  '/lib/excel_leser.rb'
 require 'spec'
-#
-#describe ExcelInputBox do
-#  it "Excel-Eingabe-Box sollte nicht leer sein" do
-#    @user_input.should_not be_nil
-#    puts "OK"
-#  end
-#  it "Excel-Eingabe-Box sollte ein string sein" do
-#    @user_input.is_a?(String)
-#  end
-#  #TODO
-#end
-#describe ExcelController do
-#  before(:each) do
-#    excel_file_name = File.dirname(File.dirname(__FILE__)) +  '/daten/' + "test.xls"
-#    #@excel_appl = ExcelController.new + excel_file_name
-#  end
-#  it "Excel-Datei sollte geoeffnet sein" do
-#    excel_file_name.should_not be_nil
-#  end
-#
-#  #TODO
-#end
+
 describe ExcelLeser do
   before(:each) do
     mappen_name = "Tabelle-4sr_a.xls"
@@ -39,8 +18,11 @@ describe ExcelLeser do
   end
 
   it "sollte auf die Methode 'zeile' reagieren" do
-
     @el.zeile(22).should_not be_nil
+  end
+
+  it "sollte bei 'zeile' einen Hash zurückgeben" do
+    @el.zeile(22).is_a?(Hash).should be_true
   end
 
   it "sollte Zeile 22 korrekt einlesen" do
