db33d6b72e420376fe71cea00c2e72f65e5971c6
Sven Suska <sven838git@suska.org>
2009-08-05 18:12:06 +0200

    spec für FormFiller schon ganz ordentlich...

--- spec/form_filler_spec.rb
+++ spec/form_filler_spec.rb
@@ -3,6 +3,8 @@
 
 require 'form_filler'
 
+#require 'tasten_sender'
+
 describe FormFiller do
   before(:each) do
     mappen_name = "sr38a_entkernt.xls"
@@ -13,13 +15,40 @@ describe FormFiller do
   end
 
   after(:each) do
-    @ff.ergebnis_anfordern
+    @ff.excel_beenden
   end
 
-
   it "sollte nicht abstürzen" do
-    zeile = {:name=>"Max Peter"}
+    name = "Max Peter"
+    zeile = {:name => name}
     @ff.fill(zeile)
+  end
+
+  it "sollte Namen korrekt eintragen" do
+    name = "Max Peter"
+    zeile = {:name => name, :bruttogehalt => 2000}
+    @ff.fill(zeile)
+    #TastenSender.new().sende_tasten('Microsoft Excel', nil).should == true
+
+#    @ff.ergebnis_anfordern
+    erg = @ff.vb_senden("Abfrage_Feld_Name")
+    erg.should == name
+  end
+
+  it "sollte Kinderfreibetrag korrekt eintragen" do
+    @ff.fill(:kinder_fb => 2, :bruttogehalt => 2000)
+    @ff.vb_senden("Abfrage_Feld_kinderfreibetraege").should == 2
+  end
+
+  it "sollte Verzichts-Betrag korrekt eintragen" do
+    betrag = 43.50
+    @ff.fill(:verzicht_betrag => betrag, :bruttogehalt => 2000)
+    @ff.vb_senden("Abfrage_Feld_nvz_betrag").should == 43.50
+  end
+
+  it "sollte MinijobOK korrekt eintragen" do
+    @ff.fill(:minijob_ok => true, :bruttogehalt => 2000)
+    @ff.vb_senden("Abfrage_Feld_Minijob").should == true
   end
 end
 
