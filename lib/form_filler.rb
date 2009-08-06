e3f35a5051e92598af278830e3d788917c4e835b
Enno <too_lame_for_a_name@gmx.net>
2009-08-06 19:21:14 +0200

    blatt "persoenliche daten" wird gefuellt und getestet

--- lib/form_filler.rb
+++ lib/form_filler.rb
@@ -4,25 +4,110 @@ require 'win32ole'
 
 require 'grundlage'
 require 'excel_controller'
+require 'tasten_sender'
 
 #constant definition
 #FILE_PATH = File.dirname(File.dirname(__FILE__)).gsub('\\', '/') +  '/daten/'
 
-class FormFiller
-  attr_reader :excel_appl
-
-  def initialize(dateiname, start_proc_name)
-    WIN32OLE.codepage = WIN32OLE::CP_UTF8 #zeichen als unicode verarbeiten
-    @excel_controller = ExcelController.new(dateiname)
-    @excel_controller.open_excel_file(dateiname)
+class FormFiller 
+ 
+  def initialize(pfad, dateiname, start_proc_name)
+    @proc_name = start_proc_name
+    @datei_name = dateiname
+    @excel_controller = ExcelController.new(pfad + dateiname)
+    @excel_controller.open_excel_file(pfad + dateiname)
     @xlapp = @excel_controller.excel_appl
+    @masken_controller = TastenSender.new()
+    @feld_kinderlos_aktiv = true
   end
 
-  def fill(zeile)
-
+  def maske_oeffnen
+    #@xlapp.Run "#{@datei_name}!#{@proc_name}"
+    @masken_controller.sende_tasten('Microsoft Excel', "%{F8}#{@proc_name}%{a}")
   end
 
-  def ergebnis_anfordern
+  def naechstes_feld(anzahl)
+    anzahl_tabs = "{TAB}"
+    anzahl_tabs = anzahl_tabs*anzahl
+    TastenSender.new().sende_tasten('Microsoft Excel', "#{anzahl_tabs}")
+  end
+  def eingabe_bestaetigen
+    TastenSender.new().sende_tasten('Microsoft Excel', "{ENTER}", :wartezeit => 1)
+  end
+
+  def maske_fuellen(zeile)
+    maske_oeffnen
+    @masken_fueller = TastenSender.new()
+    #Blatt 1
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:name]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:bruttogehalt]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:freibetrag]}")
+    naechstes_feld(1)
+    if zeile[:k_vers_art] == "p"
+      naechstes_feld(1)
+      @masken_fueller.sende_tasten('Microsoft Excel', " ")
+      feld_kinderlos_aktiv = false
+    else
+      @masken_fueller.sende_tasten('Microsoft Excel', " ")
+      naechstes_feld(1)
+      feld_kinderlos_aktiv = true
+    end 
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:steuerklasse]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:kinder_fb]}")
+    naechstes_feld(1)
+    if zeile[:kirchensteuer] == "n"
+      @masken_fueller.sende_tasten('Microsoft Excel', " ")
+    end
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:bland_wohnsitz]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:bland_arbeit]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:berufsgruppe]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:durchfuehrungsweg]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:pausch_steuer40b]}")
+    naechstes_feld(1)
+    @masken_fueller.
+      sende_tasten('Microsoft Excel', "#{zeile[:minijob_ok ]}")
+    naechstes_feld(1)
+    if feld_kinderlos_aktiv == true
+      if zeile[:kinderlos] == "j"
+        @masken_fueller.sende_tasten('Microsoft Excel', " ")
+      end
+      naechstes_feld(1)
+    end
+    eingabe_bestaetigen
+    eingabe_bestaetigen
+    naechstes_feld(1)
+    # @masken_fueller.sende_tasten('Microsoft Excel', "%j", :wartezeit => 1)
+    #Blatt 2
+  end
+
+  def vb_senden(vb_abfrage)
+    @xlapp.Run "#{@datei_name}!#{vb_abfrage}" #(vb_abfrage)
+  end
+
+  def maske_schliessen
+    @xlapp.ActiveWorkbook.Close
+  end
+
+  def excel_beenden
     @excel_controller.quit_excel
   end
 
--- spec/form_filler_spec.rb
+++ spec/form_filler_spec.rb
@@ -7,48 +7,45 @@ require 'form_filler'
 
 describe FormFiller do
   before(:each) do
-    mappen_name = "sr38a_entkernt.xls"
+    mappen_name = "sr38a_entkernt_test.xls"
     mappen_pfad = File.dirname(File.dirname(__FILE__)) +  "/daten/"
-    dateiname = mappen_pfad + mappen_name
+    # dateiname = mappen_pfad + mappen_name
     start_proc_name = "Entgeltumwandlungsrechner_starten"
-    @ff = FormFiller.new(dateiname, start_proc_name)
+    @ff = FormFiller.new(mappen_pfad, mappen_name, start_proc_name)
   end
 
   after(:each) do
+    @ff.maske_schliessen
     @ff.excel_beenden
   end
 
-  it "sollte nicht abstürzen" do
-    name = "Max Peter"
-    zeile = {:name => name}
-    @ff.fill(zeile)
-  end
+
 
   it "sollte Namen korrekt eintragen" do
-    name = "Max Peter"
-    zeile = {:name => name, :bruttogehalt => 2000}
-    @ff.fill(zeile)
+    zeile = {:name => "Max Peter", :bruttogehalt => 2000, :k_vers_art => "p",
+      :steuerklasse => "II"}
+    @ff.maske_fuellen(zeile)
     #TastenSender.new().sende_tasten('Microsoft Excel', nil).should == true
 
-#    @ff.ergebnis_anfordern
-    erg = @ff.vb_senden("Abfrage_Feld_Name")
-    erg.should == name
+    #    @ff.ergebnis_anfordern
+    @ff.vb_senden("Abfrage_Feld_name").should == "Max Peter"
   end
 
   it "sollte Kinderfreibetrag korrekt eintragen" do
-    @ff.fill(:kinder_fb => 2, :bruttogehalt => 2000)
+    @ff.maske_fuellen(:bruttogehalt => 2000, :k_vers_art => "g", 
+      :kinder_fb => 2, :kinderlos => "j")
     @ff.vb_senden("Abfrage_Feld_kinderfreibetraege").should == 2
   end
-
-  it "sollte Verzichts-Betrag korrekt eintragen" do
-    betrag = 43.50
-    @ff.fill(:verzicht_betrag => betrag, :bruttogehalt => 2000)
-    @ff.vb_senden("Abfrage_Feld_nvz_betrag").should == 43.50
-  end
-
-  it "sollte MinijobOK korrekt eintragen" do
-    @ff.fill(:minijob_ok => true, :bruttogehalt => 2000)
-    @ff.vb_senden("Abfrage_Feld_Minijob").should == true
-  end
+  #
+  #  it "sollte Verzichts-Betrag korrekt eintragen" do
+  #    betrag = 43.50
+  #    @ff.maske_fuellen(:verzicht_betrag => betrag, :bruttogehalt => 2000)
+  #    @ff.vb_senden("Abfrage_Feld_nvz_betrag").should == 43.50
+  #  end
+  #
+  #  it "sollte MinijobOK korrekt eintragen" do
+  #    @ff.maske_fuellen(:minijob_ok => true, :bruttogehalt => 2000)
+  #    @ff.vb_senden("Abfrage_Feld_Minijob").should == true
+  #  end
 end
 
